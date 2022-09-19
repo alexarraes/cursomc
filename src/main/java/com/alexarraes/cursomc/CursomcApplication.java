eef00ca4a5923b30b9deeb09daa3353f5207a384
Alex PC Brisa <alex-junior1@hotmail.com>
2022-09-19 10:22:13 -0300

    Paginacao com parametros opcionais na requisição

--- src/main/java/com/alexarraes/cursomc/CursomcApplication.java
+++ src/main/java/com/alexarraes/cursomc/CursomcApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.PageRequest;
 
 import com.alexarraes.cursomc.domain.Categoria;
 import com.alexarraes.cursomc.domain.Cidade;
@@ -70,6 +71,12 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		Categoria cat1 = new Categoria(null,"Informatica");
 		Categoria cat2 = new Categoria(null,"Escritório");
+		Categoria cat3 = new Categoria(null,"Cama mesa e Banho");
+		Categoria cat4 = new Categoria(null,"Eletronicos");
+		Categoria cat5 = new Categoria(null,"Jardinagem");
+		Categoria cat6 = new Categoria(null,"Decoracao");
+		Categoria cat7 = new Categoria(null,"Perfumaria");
+
 		
 		Produto p1 = new Produto(null, "computador", 2000.00);
 		Produto p2 = new Produto(null,"impressora", 800.00);
@@ -82,7 +89,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 				
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 		Estado est1 = new Estado(null,"Minas Gerais");
--- src/main/java/com/alexarraes/cursomc/services/CategoriaService.java
+++ src/main/java/com/alexarraes/cursomc/services/CategoriaService.java
@@ -5,6 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Service;
 
 import com.alexarraes.cursomc.domain.Categoria;
@@ -52,4 +56,12 @@ public class CategoriaService {
 		return repo.findAll();
 	}
 
+	public Page<Categoria> findPage(Integer page,Integer linesPerPage,
+			String orderBy, String direction){
+		//paginacao
+		PageRequest pageRequest = 
+				PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
 }
