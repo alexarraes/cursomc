2c29bc8accad9b0b1e9dc2ad47f442cd53a89165
Alex PC Brisa <alex-junior1@hotmail.com>
2022-12-28 08:26:26 -0300

    Validacao de e-mail não duplicado, porem erro estoura

--- src/main/java/com/alexarraes/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/alexarraes/cursomc/dto/ClienteDTO.java
@@ -8,30 +8,31 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.alexarraes.cursomc.domain.Cliente;
+import com.alexarraes.cursomc.services.validation.ClienteUpdate;
 
-public class ClienteDTO implements Serializable  {
+@ClienteUpdate
+public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
 	private Integer id;
 	
-	@NotEmpty(message = "Preenchimento obrigatorio")
-	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 80 caracteres")
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
 	
-	@NotEmpty(message = "Preenchimento obrigatorio")
-	@Email(message = "Email Invalido")
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="Email inválido")
 	private String email;
 	
 	public ClienteDTO() {
-		
 	}
-	
+
 	public ClienteDTO(Cliente obj) {
 		id = obj.getId();
 		nome = obj.getNome();
 		email = obj.getEmail();
 	}
-
+	
 	public Integer getId() {
 		return id;
 	}
@@ -55,7 +56,4 @@ public class ClienteDTO implements Serializable  {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
-
 }
